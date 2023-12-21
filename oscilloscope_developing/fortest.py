7ae18fcba860a46c818e399c5b656de67113d3dd
TIANCHENG <95949898+TCXiong@users.noreply.github.com>
2023-12-20 23:37:13 -0600

    Add files via upload

---
+++ oscilloscope_developing/fortest.py
@@ -0,0 +1,9 @@
+# does not work!
+
+
+# import pyvisa as visa
+# rm = visa.ResourceManager()
+# scope = rm.open_resource("USB0::0x2A8D::0x9009::MY53120104::INSTR")
+# unit = scope.query("MEASU:MEAS1:UNITS?")
+# print(unit)
+
